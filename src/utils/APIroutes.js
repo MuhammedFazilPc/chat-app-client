e955975bd0737ea004aadb01a4fd3a5f440c9892
Fazil <136937292+MuhammedFazilPc@users.noreply.github.com>
2024-05-06 23:13:24 +0530

    Update APIroutes.js

--- src/utils/APIroutes.js
+++ src/utils/APIroutes.js
@@ -1,4 +1,4 @@
-const host='https://chat-app-server-gold-ten.vercel.app/'
+export const host='http://16.171.114.126:5000/'
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`
 export const setAvatarRoute=`${host}/api/auth/setAvatar`
